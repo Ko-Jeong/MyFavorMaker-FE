fa283196a94e30d6c71d93ea0e04f5d63ccd94c5
HeejuKo <heezuko@gmail.com>
2026-08-11 20:03:54 +0900

    ✏️ Fix: KickFlip 이미지 경로 오타 수정

--- src/data/charts/kickflip.ts
+++ src/data/charts/kickflip.ts
@@ -5,11 +5,11 @@ export const kickflip: IdolGroup = {
   title: "KickFlip",
   members: [
     { name: "계훈", photoUrl: "/photos/kickflip/1.jpg" },
-    { name: "아마루", photoUrl: "/photos/kickflip/1.jpg" },
-    { name: "동화", photoUrl: "/photos/kickflip/1.jpg" },
-    { name: "주왕", photoUrl: "/photos/kickflip/1.jpg" },
-    { name: "민제", photoUrl: "/photos/kickflip/1.jpg" },
-    { name: "케이주", photoUrl: "/photos/kickflip/1.jpg" },
-    { name: "동현", photoUrl: "/photos/kickflip/1.jpg" },
+    { name: "아마루", photoUrl: "/photos/kickflip/2.jpg" },
+    { name: "동화", photoUrl: "/photos/kickflip/3.jpg" },
+    { name: "주왕", photoUrl: "/photos/kickflip/4.jpg" },
+    { name: "민제", photoUrl: "/photos/kickflip/5.jpg" },
+    { name: "케이주", photoUrl: "/photos/kickflip/6.jpg" },
+    { name: "동현", photoUrl: "/photos/kickflip/7.jpg" },
   ],
 };
