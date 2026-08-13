25f6cbd274616ef0d0a2c1f185732e171eedde83
HeejuKo <heezuko@gmail.com>
2026-08-14 00:54:56 +0900

    🔧 Chore: Xdinary Heros 멤버 데이터 정리

--- src/data/charts/xdinaryheroes.ts
+++ src/data/charts/xdinaryheroes.ts
@@ -4,7 +4,6 @@ export const xdinaryheroes: IdolGroup = {
   id: "xdinaryheroes",
   title: "Xdinary Heroes",
   members: [
-    { name: "건일", photoUrl: "/photos/xdinaryheroes/1.jpg" },
     { name: "정수", photoUrl: "/photos/xdinaryheroes/2.jpg" },
     { name: "가온", photoUrl: "/photos/xdinaryheroes/3.jpg" },
     { name: "O.de", photoUrl: "/photos/xdinaryheroes/4.jpg" },
